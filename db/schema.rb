4f31f308f62af4132b92f71c0ce17f69ed6b83af
Ryan Miller <ryanmillergm@gmail.com>
2019-06-24 18:55:05 -0600

    Changed transactions cc expiration date to string

--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 2019_06_25_004940) do
   create_table "transactions", force: :cascade do |t|
     t.bigint "invoice_id"
     t.string "credit_card_number"
-    t.date "credit_card_expiration_date"
+    t.string "credit_card_expiration_date"
     t.string "result"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
