6ecd97cd6c7a9841faede5426aa505a867aa6be4
Ryan Miller <ryanmillergm@gmail.com>
2019-06-27 11:52:50 -0600

    Adds invoice resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
       end
       resources :merchants, only: [:index, :show]
       resources :customers, only: [:index, :show]
+      resources :invoices, only: [:index, :show]
     end
   end
 end
