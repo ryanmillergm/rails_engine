acbb9cfbee834910a48008bc428e0f889dbdc1d1
Ryan Miller <ryanmillergm@gmail.com>
2019-06-27 11:52:08 -0600

    Adds returns all invoices tests

---
+++ spec/requests/api/v1/invoices/invoice_controller_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe "Invoice API" do
+  it "returns a list of all invoices" do
+    merchant = Merchant.create(name: "Bob")
+    customer = Customer.create(first_name: "Dan", last_name: "Olsen")
+    customer_2 = Customer.create(first_name: "Paul", last_name: "Johnson")
+    invoice_1 = Invoice.create(customer_id: customer.id, merchant_id: merchant.id)
+    invoice_2 = Invoice.create(customer_id: customer_2.id, merchant_id: merchant.id)
+    invoice_3 = Invoice.create(customer_id: customer_2.id, merchant_id: merchant.id)
+
+    get "/api/v1/invoices"
+
+    invoices = JSON.parse(response.body)
+    binding.pry
+    expect(invoices["data"].count).to eq(3)
+  end
+end
