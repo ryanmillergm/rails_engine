7eaa4e3dd32af3ee684527bbf8728c28e9d0a6e5
Ryan Miller <ryanmillergm@gmail.com>
2019-06-27 15:51:19 -0600

    Added merchant invoices test

---
+++ spec/requests/api/v1/merchants/merchant_invoices_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "Merchants items" do
+  it "returns a merchants invoices" do
+    customer_1 = Customer.create(first_name: "Paul", last_name: "Olsen")
+    merchant_1 = Merchant.create(name: "Bob")
+    invoice_1 = Invoice.create(customer_id: customer_1.id, merchant_id: merchant_1.id)
+    invoice_2 = Invoice.create(customer_id: customer_1.id, merchant_id: merchant_1.id)
+    invoice_3 = Invoice.create(customer_id: customer_1.id, merchant_id: merchant_1.id)
+    invoice_4 = Invoice.create(customer_id: customer_1.id, merchant_id: merchant_1.id)
+    id = merchant_1.id
+
+    get "/api/v1/merchants/:id/invoices"
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body)
+
+    expect(invoices["data"].count).to eq(4)
+  end
+
+  it "returns invoices associated with a merchant from their orders" do
+
+  end
+# GET /api/v1/merchants/:id/invoices returns a collection of invoices
+# associated with that merchant from their known orders
+end
