768bdf7f3c0982bd8fcdf44b5fc99e6a4c25280e
Ryan Miller <ryanmillergm@gmail.com>
2019-06-27 13:13:14 -0600

    Adds transactions controller and fixed relationships

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
-  has_many :invoice_items, through: :invoices
-  has_many :items, through: :invoices
-  has_many :customers, through: :invoices
-  has_many :merchants, through: :invoices
+  has_many :invoice_items, through: :invoice
+  has_many :items, through: :invoice
+  has_many :customers, through: :invoice
+  has_many :merchants, through: :invoice
 end
--- spec/requests/api/v1/transactions/transactions_spec.rb
+++ spec/requests/api/v1/transactions/transactions_spec.rb
@@ -8,21 +8,23 @@ describe "Transactions API" do
     merchant_2 = Merchant.create(name: "Chris")
     invoice_1 = Invoice.create(customer_id: customer.id, merchant_id: merchant.id)
     invoice_2 = Invoice.create(customer_id: customer_2.id, merchant_id: merchant.id)
+    invoice_3 = Invoice.create(customer_id: customer_2.id, merchant_id: merchant.id)
+    invoice_4 = Invoice.create(customer_id: customer_2.id, merchant_id: merchant_2.id)
+    invoice_5 = Invoice.create(customer_id: customer_2.id, merchant_id: merchant_2.id)
     item_1 = Item.create(name: "baseball", description: "Rawling", unit_price: 200, merchant_id: merchant.id)
     item_2 = Item.create(name: "softball", description: "Rawling", unit_price: 200, merchant_id: merchant.id)
     item_3 = Item.create(name: "golf ball", description: "Rawling", unit_price: 200, merchant_id: merchant.id)
     item_4 = Item.create(name: "golf ball", description: "Rawling", unit_price: 200, merchant_id: merchant_2.id)
     transaction_1 = Transaction.create(invoice_id: invoice_1.id, credit_card_number: "23432343234232234", credit_card_expiration_date: "01102020", result: "success")
-    transaction_2 = Transaction.create(invoice_id: invoice_1.id, credit_card_number: "23432343234232234", credit_card_expiration_date: "01102020", result: "success")
-    transaction_3 = Transaction.create(invoice_id: invoice_2.id, credit_card_number: "23432343234232234", credit_card_expiration_date: "01102020", result: "success")
-    transaction_4 = Transaction.create(invoice_id: invoice_2.id, credit_card_number: "23432343234232234", credit_card_expiration_date: "01102020", result: "success")
-    transaction_5 = Transaction.create(invoice_id: invoice_2.id, credit_card_number: "23432343234232234", credit_card_expiration_date: "01102020", result: "success")
+    transaction_2 = Transaction.create(invoice_id: invoice_2.id, credit_card_number: "23432343234232234", credit_card_expiration_date: "01102020", result: "success")
+    transaction_3 = Transaction.create(invoice_id: invoice_3.id, credit_card_number: "23432343234232234", credit_card_expiration_date: "01102020", result: "success")
+    transaction_4 = Transaction.create(invoice_id: invoice_4.id, credit_card_number: "23432343234232234", credit_card_expiration_date: "01102020", result: "success")
+    transaction_5 = Transaction.create(invoice_id: invoice_5.id, credit_card_number: "23432343234232234", credit_card_expiration_date: "01102020", result: "success")
 
     get "/api/v1/transactions"
-
-    invoices = JSON.parse(response.body)
+    transactions = JSON.parse(response.body)
 
     expect(response).to be_successful
-    expect(invoices["data"].count).to eq(5)
+    expect(transactions["data"].count).to eq(5)
   end
 end
