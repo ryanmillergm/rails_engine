e21f06e5c67a5d581fff5a60c68c6d3316ab9299
Ryan Miller <ryanmillergm@gmail.com>
2019-06-28 16:53:12 -0600

    Adds attribute block in ItemSerializer for cents to dollars

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,13 @@ class Item < ApplicationRecord
   def self.merchant_items(params)
     Item.where(merchant_id: params[:id])
   end
+
+  def self.find_item(params)
+    Item.find(params[:id])
+  end
+
+  def self.find_all_items(params)
+    Item.where(params)
+
+  end
 end
