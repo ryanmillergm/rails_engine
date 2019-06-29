a81888dea6259c5fa5b5260f61e8482427c37e11
Ryan Miller <ryanmillergm@gmail.com>
2019-06-29 11:23:09 -0600

    Adds invoice relationships and tests) exit

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe Item, type: :model do
     it { should validate_presence_of :description }
     it { should validate_presence_of :unit_price }
     it { should validate_presence_of :merchant_id }
+  end
 
+  describe  "relationships" do
+    it { should belong_to :merchant }
+    it { should have_many :invoice_items }
+    it { should have_many :invoices }
   end
 end
