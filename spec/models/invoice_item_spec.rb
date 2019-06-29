0362e285ec868c359f1b53ac6d117509f1f25907
Ryan Miller <ryanmillergm@gmail.com>
2019-06-29 11:11:41 -0600

    Adds relationships test for invoice_items

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe InvoiceItem, type: :model do
     it { should validate_presence_of :quantity }
     it { should validate_presence_of :unit_price }
   end
+
+  describe "relationships" do
+    it { should belong_to :item }
+    it { should belong_to :invoice }
+  end
 end
